8ca9a388b72b37659b84ad7e9df8bc2ce2d12a36
qthinklearn <qthink.learn@gmail.com>
2024-02-15 22:29:10 +0700

    penyelesaian perulanganwhile.js

--- perulanganwhile.js
+++ perulanganwhile.js
@@ -1,9 +1,34 @@
-let num = 0;
-while (num < 10) {
-    console.log(num);
-    num++;
-}
+//contoh 1
+// let num = 0;
+// while (num <= 10) {
+//     console.log(num);
+//     num++;
+// }
+// for (let num = 0; num <= 10; num++){
+//     console.log(num);
+// }
 
-for (let num = 0; num < 10; num++){
-    console.log(num);
+
+//contoh2
+// const PASSWORD = 'Pass123';
+// let guess = prompt('enter password');
+// while (guess !== PASSWORD) {
+//     guess = prompt ('wrong password');
+// }
+// alert('congrats...password correct');
+
+
+//contoh3
+// let input = prompt('Say Somethink Bitch!!');
+// while(true) {
+//     input = prompt(input);
+//     if (input === 'stop') break;
+// }
+// alert('OK');
+
+
+//contoh4
+for (let i = 0; i < 100; i++) {
+    console.log(i);
+    if (i === 25) break;
 }
