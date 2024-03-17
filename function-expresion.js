17f8dd6e01d8b75894d3b926e38f1965d49390cf
ardi1304 <ardiandrisuhendri@gmail.com>
2024-03-17 22:31:28 +0700

    function-expresion.js

---
+++ function-expresion.js
@@ -0,0 +1,11 @@
+const perpangkatan = function (nilai) {
+    return nilai * nilai;
+}
+console.log(perpangkatan(5));
+
+
+
+let perkalian = function(nilai1, nilai2){
+    return nilai1 * nilai2;
+}
+console.log(perkalian(5, 2));
