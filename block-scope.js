46050678d77e183880355933ff55448d52780ec9
ardi1304 <ardiandrisuhendri@gmail.com>
2024-03-17 22:00:49 +0700

    perbaikan block-scope.js

---
+++ block-scope.js
@@ -0,0 +1,15 @@
+let tinggi = 8;
+if (tinggi > 5){
+    var lebar = 10;  //catatan:utk var akan di deklasikan general
+    console.log(lebar);
+}
+console.log(tinggi);
+
+for (let index = 0; index < 10; index++) {
+    let indexNumber = index;
+    // console.log(indexNumber);
+}
+
+console.log(indexNumber);
+
+//let dan const memiliki blok nya sendiri jadi tidak bisa di akses diluar scope atau{}
